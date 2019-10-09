6171847bc60bf54a573b46c7d2b8a1a5953fc318
Pac <paskalk@gmail.com>
2019-10-09 02:08:22 +0200

    seperating query from connection

--- backend/api.js
+++ backend/api.js
@@ -1,38 +1,39 @@
 var app = require('express')();
-//var db = require('conn');
-//const sql = require('mssql');
 
-//const conn = require('../backend/conn.js');
-const helper = new require('../backend/Helper')();
-
-    
-app.get('/getUserInfo', function(request, response){
-
-    let x =  helper.runSelect('Select 1');
-    return x;
-   
-    // conn.connect().then(function () {
-
-    //     conn.query("SELECT 1").then(function (recordset) {
-            
-    //         conn.close();
-            
-    //         response.header("Access-Control-Allow-Origin", "*");
-    //         return response.json(recordset.recordsets[0]);
-    //     })
-    //         .catch(function (err) {
-    //             console.log(err);
-    //             conn.close();
-    //         });
-    // })
-    // .catch(function (err) {
-    //     console.log(err);
-    // });
+var conn = require("../backend/db");
 
 
-    
 
+app.get("/user", function(req, res){
+    var query = "select * from tbusers";
+    executeQuery(query, res);
 });
+
+//1
+app.get("/user/:userid/", function(req, res){
+    var query = `select * from tbusers where UserID = '${req.params.userid}'`;
+    executeQuery(query, res);
+});
+
+var executeQuery = function(req, res){             
+    conn.connect().then(function () {
+
+         conn.query(req).then(function (recordset) {
+            
+            conn.close();
+            
+            res.header("Access-Control-Allow-Origin", "*");
+            return res.json(recordset.recordsets[0]);
+        })
+        .catch(function (err) {
+            console.log(err);
+            conn.close();
+        });
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+}
 
 app.listen('3000'); 
 
