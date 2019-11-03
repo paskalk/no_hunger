df8312dc8ec470fce34e89540317fd9eaacc32c6
Saeed-ur-Rehman <saeed_ur.rehman0030@stud.hkr.se>
2019-11-03 19:43:11 +0100

    New form

--- frontend/src/pages/datatables/UserProfile.js
+++ frontend/src/pages/datatables/UserProfile.js
@@ -8,6 +8,19 @@ import {Button, makeStyles} from '@material-ui/core';
 import {updateUser} from "../../context/UserContext";
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import {FormControl, InputLabel, Input, FormHelperText, withStyles, Grid} from '@material-ui/core';
+
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap'
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 520
+    }
+})
 
 
 class UserProfile extends React.Component {
@@ -32,44 +45,59 @@ class UserProfile extends React.Component {
     };
 
     render() {
+        const {classes} = this.props;
 
         return (
             <Card className={'card'}>
                 <CardContent>
-                    <form onSubmit={this.handleSubmit}>
-                        <div>
-                            <TextField name="name" label="Name"/>
-                        </div>
-                        <div>
-                            <TextField name="email" label="Email"/>
-                        </div>
-
-                        <div>
-                            <TextField type="password" name="password" label="Password"/>
-                        </div>
-
-                        <div>
-                            <TextField type="mobile" name="number" label="Mobile"/>
-                        </div>
-
-                        <div>
-                            <TextField type="text" name="address" label="Address"/>
-                        </div>
 
 
-                        <div>
-                            <Button variant="contained" color="primary" className={'primary'}>
-                                Submit
-                            </Button>
+                    <Grid
+                        container
+                        spacing={0}
+                        direction="column"
+                        alignItems="center"
+                        justify="center"
+                        style={{minHeight: '100vh'}}
+                    >
 
-                        </div>
+                        <Grid item xs={3}>
+                            <FormControl className={classes.formControl}>
+                                <InputLabel htmlFor="name">Name</InputLabel>
+                                <Input id="name" aria-describedby="name-helper-text"/>
+                                <FormHelperText id="name-helper-text">Your full name</FormHelperText>
 
-                    </form>
+                                <InputLabel htmlFor="email">Email address</InputLabel>
+                                <Input id="email" aria-describedby="email-helper-text"/>
+                                <FormHelperText id="email-helper-text">We'll never share your email.</FormHelperText>
+
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input id="password" aria-describedby="password-helper-text"/>
+                                <FormHelperText id="password-helper-text">Your password.</FormHelperText>
+
+                                <InputLabel htmlFor="mobile">Your mobile</InputLabel>
+                                <Input id="mobile" aria-describedby="mobile-helper-text"/>
+                                <FormHelperText id="mobile-helper-text">We'll never share your email.</FormHelperText>
+
+                                <InputLabel htmlFor="address">Address</InputLabel>
+                                <Input id="address" aria-describedby="address-helper-text"/>
+                                <FormHelperText id="address-helper-text">Please fill your address.</FormHelperText>
+
+
+                                <Button variant="contained" color="primary" className={'primary'}>
+                                    Submit
+                                </Button>
+
+                            </FormControl>
+                        </Grid>
+
+                    </Grid>
+
                 </CardContent>
             </Card>
         )
     }
 }
 
-export default UserProfile;
+export default withStyles(styles)(UserProfile);
 
