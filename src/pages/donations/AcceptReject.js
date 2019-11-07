902adfbabec71f83efd8da06bb6b1a111e71e672
Saeed-ur-Rehman <saeed_ur.rehman0030@stud.hkr.se>
2019-11-07 19:55:24 +0100

    Added dontations fetch

--- src/pages/donations/AcceptReject.js
+++ src/pages/donations/AcceptReject.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Grid} from "@material-ui/core";
+import {useState, useEffect} from 'react';
+import {updateProfile} from "../../context/UserContext";
 
 // components
 import PageTitle from "../../components/PageTitle";
@@ -10,17 +12,42 @@ import Table from "../dashboard/components/Table/TableAcceptReject";
 import mock from "../dashboard/mock";
 
 
-export default function AcceptReject(props) {
-    return(
-        <>
-            <PageTitle title="Accept Reject Donations" />
-            <Grid container spacing={4}>
-                <Grid item xs={12}>
-                    <Widget title="List">
-                        <Table data={mock.donations} />
-                    </Widget>
+export default class AcceptReject extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            donations: []
+        }
+    }
+
+    componentDidMount() {
+        var urlpath = process.env.NODE_ENV == "development" ? process.env.REACT_APP_URL_PATH : "";
+
+        fetch(`http://localhost:3030/api/getActiveDonations/1`)
+            .then(response => response.json())
+            .then((response) => {
+                if (response) {
+                    this.setState({donations: response});
+                    console.log('Donations fetched : ' , response);
+                }
+            })
+    }
+
+    render() {
+        let { donations } = this.state;
+        if(donations.length < 1) donations = mock.donations;
+
+        return (
+            <>
+                <PageTitle title="Accept Reject Donations"/>
+                <Grid container spacing={4}>
+                    <Grid item xs={12}>
+                        <Widget title="List">
+                            <Table data={donations}/>
+                        </Widget>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </>
-    )
+            </>
+        )
+    }
 }
